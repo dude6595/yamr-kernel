5594948db52cac4ad0c6d7175130032cdf890120
bonuzzz <bonuzzz@gmail.com>
2017-06-22 17:34:44 +0300

    initial commit

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 SMP PREEMPT Thu Jun 22 02:45:07 MSK 2017"
+#define LINUX_COMPILE_BY "build"
+#define LINUX_COMPILE_HOST "android"
+#define LINUX_COMPILER "gcc version 4.8 (GCC) "
